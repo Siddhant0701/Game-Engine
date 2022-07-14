74e523224fb590c7f558df602a3540d20040edeb
NinePiece2 <romit.sagu@hotmail.com>
2022-07-14 15:36:49 -0400

    Window Class Completed and Test Ready

--- Game-Engine/app/src/main/java/game/engine/Alpha/Window.java
+++ Game-Engine/app/src/main/java/game/engine/Alpha/Window.java
@@ -1,4 +1,4 @@
-package game.engine.Alpha;
+package game.engine.alpha;
 
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
@@ -18,6 +18,7 @@ public class Window {
 
     private int height, width;
     private String title;
+    private long glfwWindow;
 
     private static Window window = null;
 
@@ -36,7 +37,61 @@ public class Window {
     }
 
     public void run (){
-       //System.out.println("Hello LWJGL v=" + Version.getVersion);
+       System.out.println("Hello LWJGL version=" + Version.getVersion() + " !");
+
+       init();
+       loop();
+    }
+
+    public void init (){
+        // Setup an Error Callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+
+        if (!glfwInit()){
+            throw new IllegalStateException("Unable to initalize GLFW");
+        }
+
+        //  Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the Window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create the GLFW Window.");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable V-Sync
+        glfwSwapInterval(1);
+
+        // Make the Window Visable
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    public void loop (){
+        while (!glfwWindowShouldClose(glfwWindow)){
+            // Poll Events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 
 }
--- Game-Engine/app/src/main/java/game/engine/Main.java
+++ Game-Engine/app/src/main/java/game/engine/Main.java
@@ -1,7 +1,11 @@
 package game.engine;
 
+import game.engine.alpha.Window;
+
 public class Main {
     public static void main (String [] args){
         System.out.println("Heloo");
+        Window window = Window.get();
+        window.run();
     }
 }
